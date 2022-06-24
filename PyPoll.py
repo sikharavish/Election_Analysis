83fe11a1e84b480da6d0f571a36f04071ac37c80
sikharavish <105684346+sikharavish@users.noreply.github.com>
2022-06-24 10:31:48 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -21,6 +21,14 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"----------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"---------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = (float (votes)/ float(total_votes)) * 100
@@ -31,7 +39,10 @@ with open(file_to_load) as election_data:
             winning_name = candidate_name
         #print(winning_count)
         #print(winning_percentage)
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"
+        print(candidate_results)
+        txt_file.write(candidate_results)
         for candidate in candidate_votes:
             winning_candidate_summary = (
             f"----------------------------------\n"
@@ -40,4 +51,5 @@ with open(file_to_load) as election_data:
             f"Winning Percentage: {winning_percentage:.1f}%\n"
             f"--------------------------------------\n"
             )
-    print(winning_candidate_summary)   
+    #print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)   
